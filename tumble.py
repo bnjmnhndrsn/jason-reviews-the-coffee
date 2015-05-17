aeabc980f50a559ba716607ccbf6ec0605b2eaad
Ben <benjamin.c.henderson@gmail.com>
2015-05-16 23:23:49 -0400

    update config vars

--- tumble.py
+++ tumble.py
@@ -52,7 +52,7 @@ def post_to_tumblr(client, params):
     client.create_quote("jasonreviewscoffee", **params)         
     
 
-hc = hypchat.HypChat(HIPCHAT_TOKEN)
+hc = hypchat.HypChat(os.environ['HIPCHAT_TOKEN'])
 
 ROOM_ID = 44415
 USER_IDS = (79780,)
@@ -72,10 +72,10 @@ filtered = filter_results(history, [filter_by_id, filter_by_text])
 print 'item count: {0}'.format(len(filtered))
 
 client = pytumblr.TumblrRestClient(
-    TUMBLR_CONSUMER_KEY,
-    TUMBLR_SECRET_KEY,
-    TUMBLR_OAUTH_TOKEN,
-    TUMBLR_OAUTH_SECRET,
+    os.environ['TUMBLR_CONSUMER_KEY'],
+    os.environ['TUMBLR_SECRET_KEY'],
+    os.environ['TUMBLR_OAUTH_TOKEN'],
+    os.environ['TUMBLR_OAUTH_SECRET'],
 )
 
 for item in filtered:
