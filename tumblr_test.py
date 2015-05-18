6714d4c1836d6f6aebe4ed63b862cbbc3141b0b7
Ben Henderson <ben@nestio.com>
2015-05-18 18:30:57 -0400

    add in test

---
+++ tumblr_test.py
@@ -0,0 +1,18 @@
+def post_to_tumblr(client, params):
+    return client.create_quote("jasonreviewscoffee", **params)         
+    
+client = pytumblr.TumblrRestClient(
+    os.environ['TUMBLR_CONSUMER_KEY'],
+    os.environ['TUMBLR_SECRET_KEY'],
+    os.environ['TUMBLR_OAUTH_TOKEN'],
+    os.environ['TUMBLR_OAUTH_SECRET'],
+)
+
+obj = post_to_tumblr(client, {
+    'state': 'published',
+    'tags': ['jasonreviewsthecoffee'],
+    'quote': "TEST TEST TEST",
+    'source': 'Jason'
+})
+
+print obj
